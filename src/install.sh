92a1cd01906c21b9d3a9f31f1d96374f45bbd98f
kyle.a.dotterrer.mil <kyle.a.dotterrer.mil@army.mil>
2024-10-09 14:43:04 +0000

    add hosts

--- src/install.sh
+++ src/install.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 #
 # install.sh
+#
+# This script performs the following operations:
+# - Downloads and installs the root CA for AIDE clusters in the trusted store
+# - Writes the required entries to the hosts file
 
 # Download and install the mkcert binary
 wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.4/mkcert-v1.4.4-linux-amd64 && \
     chmod +x mkcert-v1.4.4-linux-amd64 && \
     sudo mv mkcert-v1.4.4-linux-amd64 /usr/local/bin/mkcert
 
+# Download the root CA certificate
+wget https://raw.githubusercontent.com/aide-team/aide-utils/refs/heads/main/files/rootCA.pem
+
+# Move the certificate to the proper location
+mkdir -p $(mkcert -CAROOT) && \
+    mv rootCA.pem $(mkcert -CAROOT)
+
+# Install the certificate in the trusted store
+mkcert -install
+
+# 
