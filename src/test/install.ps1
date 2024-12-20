32031f7c556d6bcb70b034595454ed28aacae92b
Ferrell, Angus M CPT USARMY AFC AI2C (USA) <angus.m.ferrell.mil@army.mil>
2024-12-20 11:04:52 -0500

    update ps1

--- src/test/install.ps1
+++ src/test/install.ps1
@@ -10,7 +10,11 @@ Import-Certificate -FilePath "$env:USERPROFILE\Downloads\MyCustomCA.crt" -CertSt
 
 # Define paths
 $HostsFilePath = "C:\Windows\System32\drivers\etc\hosts"
+$BackupHostsFilePath = "C:\Windows\System32\drivers\etc\hosts.bak"
 $HostsTxtUrl = "https://raw.githubusercontent.com/angusmf1/aide-utils/refs/heads/main/files/test/hosts.txt"
+
+# Back up the original hosts file
+Copy-Item -Path $HostsFilePath -Destination $BackupHostsFilePath -Force
 
 # Download the hosts.txt content
 $HostsContent = Invoke-WebRequest -Uri $HostsTxtUrl -UseBasicParsing | Select-Object -ExpandProperty Content
@@ -24,4 +28,4 @@ Add-Content -Path $HostsFilePath -Value $CleanHostsContent
 # Add a blank line at the end of the hosts file
 Add-Content -Path $HostsFilePath -Value ""
 
-Write-Host "Hosts file updated successfully. 
+Write-Host "Hosts file updated successfully. Backup saved at: $BackupHostsFilePath"
