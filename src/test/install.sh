27068503741141c37a614c15b98d5cb4074b17b0
Ferrell, Angus M CPT USARMY AFC AI2C (USA) <angus.m.ferrell.mil@army.mil>
2024-12-20 09:17:32 -0500

    update test/hosts

--- src/test/install.sh
+++ src/test/install.sh
@@ -12,11 +12,11 @@ wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.4/mkcert-v1.4.
     sudo mv mkcert-v1.4.4-linux-amd64 /usr/local/bin/mkcert
 
 # Download the root CA certificate
-wget https://raw.githubusercontent.com/angusmf1/aide-utils/refs/heads/main/files/test/rootCA.crt
+wget https://raw.githubusercontent.com/angusmf1/aide-utils/refs/heads/main/files/test/MyCustomCA.crt
 
 # Move the certificate to the proper location
 mkdir -p $(mkcert -CAROOT) && \
-    mv rootCA.crt $(mkcert -CAROOT)
+    mv MyCustomCA.crt $(mkcert -CAROOT)
 
 # Install the certificate in the trusted store
 mkcert -install
@@ -24,6 +24,6 @@ mkcert -install
 # Update the hosts file; ensure that CRLF is converted to LF for UNIX
 sudo cp /etc/hosts /etc/hosts.bak && \
     wget -q -O - https://raw.githubusercontent.com/angusmf1/aide-utils/refs/heads/main/files/test/hosts.txt | tr -d '\r' | sudo tee -a /etc/hosts > /dev/null && \
-    echo ''0 | sudo tee -a /etc/hosts > /dev/null
+    echo '' | sudo tee -a /etc/hosts > /dev/null
 
 exit 0
