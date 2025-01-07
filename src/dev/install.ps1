825929a41c18992b4a71f9be832152deb2d1a9bb
Ferrell, Angus M CPT USARMY AFC AI2C (USA) <angus.m.ferrell.mil@army.mil>
2025-01-07 09:57:04 -0500

    update dev

--- src/dev/install.ps1
+++ src/dev/install.ps1
@@ -1,28 +1,31 @@
-# install.ps
-#
 # Install AIDE configuration.
+ 
+# Download the root CA certificate.
+Write-Output "[+] Downloading root CA certificate..."
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/angusmf1/aide-utils/refs/heads/main/files/dev/rootCA.crt" -OutFile "$env:USERPROFILE\Downloads\rootCA.crt"
+ 
+# Install the root CA certificate.
+Write-Output "[+] Preparing root CA..."
+Import-Certificate -FilePath "$env:USERPROFILE\Downloads\rootCA.crt" -CertStoreLocation "Cert:\CurrentUser\Root\"
 
-# Download and install the mkcert binary;
-# the binary is kept in Downloads to avoid admin requirement
-echo "Downloading mkcert..."
-Invoke-WebRequest -Uri "https://github.com/FiloSottile/mkcert/releases/download/v1.4.4/mkcert-v1.4.4-windows-amd64.exe" -OutFile "$env:USERPROFILE\Downloads\mkcert.exe"
+# Define paths
+$HostsFilePath = "C:\Windows\System32\drivers\etc\hosts"
+$BackupHostsFilePath = "C:\Windows\System32\drivers\etc\hosts.bak"
+$HostsTxtUrl = "https://raw.githubusercontent.com/angusmf1/aide-utils/refs/heads/main/files/dev/hosts.txt"
 
-# Download the root CA certificate
-echo "Downloading root CA certificate..."
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/aide-team/aide-utils/refs/heads/main/files/dev/rootCA.pem" -OutFile "$env:USERPROFILE\Downloads\rootCA.pem"
+# Back up the original hosts file
+Copy-Item -Path $HostsFilePath -Destination $BackupHostsFilePath -Force
 
-# Get the mkcert CA root folder and move the root CA certificate there
-echo "Preparing root CA..."
-$caRootPath = & "$env:USERPROFILE\Downloads\mkcert.exe" -CAROOT
-if (-not (Test-Path -Path $caRootPath)) {
-    New-Item -Path $caRootPath -ItemType Directory
-}
-Move-Item -Force -Path "$env:USERPROFILE\Downloads\rootCA.pem" -Destination $caRootPath
+# Download the hosts.txt content
+$HostsContent = Invoke-WebRequest -Uri $HostsTxtUrl -UseBasicParsing | Select-Object -ExpandProperty Content
 
-# Install the root CA certificate
-echo "Installing root CA..."
-& "$env:USERPROFILE\Downloads\mkcert.exe" -install
+# Remove any CRLF characters (if necessary)
+$CleanHostsContent = $HostsContent -replace "`r", ""
 
-# Remove downloads
-echo "Cleaning up..."
-Remove-Item "$env:USERPROFILE\Downloads\mkcert.exe"
+# Append the downloaded content to the hosts file
+Add-Content -Path $HostsFilePath -Value $CleanHostsContent
+
+# Add a blank line at the end of the hosts file
+Add-Content -Path $HostsFilePath -Value ""
+
+Write-Host "Hosts file updated successfully. Backup saved at: $BackupHostsFilePath"
